9b5d621371136a81209094036c840f4e8a424bc4
Daniel Carmona <danielcarmona51@aragon.unam.mx>
2022-05-22 08:55:54 -0500

    Agrege los credtiso

--- modules/sacar_distancias.py
+++ modules/sacar_distancias.py
@@ -1,3 +1,6 @@
+## Información relacionada a la busqueda del algoritmo fue hecha por Victor Sanz de su canal de youtube
+## Podemos encontrar una explicación en su video https://www.youtube.com/watch?v=w84P5Coifek
+
 import pandas as pd
 import networkx as nx
 
